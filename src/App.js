e8012ea84d2e42ff5e2031faf94b4aa0128f7e22
Tiara Feaster <tfeaster21@gmail.com>
2021-07-07 14:39:16 -0400

    added search engine feature

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>Weather App - React Version pt.1</h1>
         <Weather />
       </header>
     </div>
--- src/Weather.js
+++ src/Weather.js
@@ -1,10 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
+import "./weather.css";
 
 function Weather() {
+  let [city, setCity] = useState();
+  let [temp, setTemp] = useState();
+
+  function showTemp(response) {
+    setTemp(
+      <div>
+        <h2>Here are your weather details for {city} </h2>
+        <ul>
+          <li> Temperature: {Math.round(response.data.main.temp)} ℃ </li>
+          <li> Description: {response.data.weather[0].description} </li>
+          <li>Humidity: {response.data.main.humidity} % </li>
+          <li>Wind: {response.data.wind.speed} km/h</li>
+          <li>
+            {" "}
+            <img
+              src={`http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`}
+              alt={response.data.weather[0].description}
+            />
+          </li>
+        </ul>
+      </div>
+    );
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    let apiKey = "5423fee45fccae4c3cd5d7b43daf88ad";
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+    axios.get(url).then(showTemp);
+  }
+
+  function updateCity(event) {
+    setCity(event.target.value);
+  }
+
   return (
-    <div className="weather">
-      <h2> Weather App - React Version </h2>
-    </div>
+    <form onSubmit={handleSubmit}>
+      <input
+        type="search"
+        placeholder="Please enter a city..."
+        onChange={updateCity}
+      />
+      <input type="submit" value="Search" />
+      <h3>{temp}</h3>
+    </form>
   );
 }
 
