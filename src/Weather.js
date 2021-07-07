20b07f98efb369bf82d0b3324bb8cef7245b4c39
Tiara Feaster <tfeaster21@gmail.com>
2021-07-07 13:38:10 -0400

    initial commit

---
+++ src/Weather.js
@@ -0,0 +1,12 @@
+import React from "react";
+import axios from "axios";
+
+function Weather() {
+  return (
+    <div className="weather">
+      <h2> Weather App </h2>
+    </div>
+  );
+}
+
+export default Weather;
