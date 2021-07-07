26d913445320a101ebcfe52cde231df421904bb6
Tiara Feaster <tfeaster21@gmail.com>
2021-07-07 15:13:44 -0400

    changed colors

--- src/App.js
+++ src/App.js
@@ -7,19 +7,17 @@ function App() {
       <header className="App-header">
         <h1>Weather App - React Version pt.1</h1>
         <Weather />
-      </header>
-      <footer>
-        <h3>
+        <h5>
           This project is open source and was coded by{" "}
           <a
             href="https://github.com/tiarafeaster/weather-react"
             target="_blank"
             rel="noreferrer"
           >
-            Tiara Feaster{" "}
+            ✊🏾 Tiara Feaster 🏳️‍🌈{" "}
           </a>
-        </h3>
-      </footer>
+        </h5>
+      </header>
     </div>
   );
 }
